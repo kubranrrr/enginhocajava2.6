58799660dd5e89481bf341c43ad16bdf18ad5251
kubra <kubraak3333@hotmail.com>
2022-10-07 00:49:27 +0300

    inheritance projesi
    2.dersin inheritance projesi

---
+++ inheritance/src/inheritance/EmployerManager.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class EmployerManager extends PersonManager {
+	
+	public void BestEmployer() {
+		System.out.println("Ayın elemanı getirildi");
+	}
+
+}
---
+++ inheritance/src/inheritance/Main.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Customer customer = new Customer();
+		Employer employer = new Employer();
+		
+		EmployerManager employerManager = new EmployerManager();
+		CustomerManager customerManager = new CustomerManager();
+		
+		
+
+	}
+
+}
